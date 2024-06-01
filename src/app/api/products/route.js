04ac947d5a2e6c38a1b0434203143f1de314c917
SamiSelx <samifcb14@gmail.com>
2024-06-02 01:22:19 +0200

    Route POST

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,18 @@
 
-import {products} from '@/app/productData'
+import {products,menProduct,womenProdut,kidProduct} from '@/app/productData'
 
 export async function GET(){
     return Response.json(products)
+}
+
+export async function POST(request){
+    let newProduct = await request.json();
+    products.push(newProduct)
+    // switch case for add product on men | women | kid
+    return new Response(JSON.stringify(products,{
+        headers:{
+            "content-type":"application/json"
+        },
+        status:201
+    }))
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,15 +10,25 @@ const productType = ["Men","Women","Kid"]
 export default async function Home() {
   return (
     <>
-      <Main/>
-      {productType.map(async (p)=>{
-       const response = await fetch(`${process.env.URL_HOST}/api/products/${p.toLowerCase()}`)
-       const products = await response.json()
-        return <Product title={p+"'s"} desc={"Details to details is what makes Hexashop different from the other themes."} products={products}/>
+      <Main />
+      {productType.map(async (p) => {
+        const response = await fetch(
+          `${process.env.URL_HOST}/api/products/${p.toLowerCase()}`
+        );
+        const products = await response.json();
+        return (
+          <Product
+            title={p + "'s"}
+            desc={
+              "Details to details is what makes Hexashop different from the other themes."
+            }
+            products={products}
+          />
+        );
       })}
-      <ExploreProduct/>
-      <SocialMedia/>
-      <Subscribe/>
+      <ExploreProduct />
+      <SocialMedia />
+      <Subscribe />
     </>
   );
 }
